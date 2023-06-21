493a7734e61b02dc73a8b7e58891719e22bb7eb5
Chady <chady.ayoub@hotmail.com>
2023-06-21 12:13:21 +0300

    started second screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:assesment/api/dio_client.dart';
 import 'package:assesment/config/app_router.dart';
+import 'package:assesment/screens/forecast_screen.dart';
+import 'package:assesment/screens/upcoming_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,12 +15,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => DioClient())],
-      child: MaterialApp(
+        providers: [ChangeNotifierProvider(create: (_) => DioClient())],
+        child: MaterialApp(
           title: 'Weather',
           theme: ThemeData().copyWith(primaryColor: Colors.black),
-          onGenerateRoute: AppRouter.onGenerateRoute,
-          initialRoute: RouteNames.forecastScreen),
-    );
+          // onGenerateRoute: AppRouter.onGenerateRoute,
+          // initialRoute: RouteNames.forecastScreen),
+          home: DefaultTabController(
+            length: 2,
+            child: Scaffold(
+              appBar: AppBar(
+                title: Image.network(defaultImage, width: 50, height: 50),
+                backgroundColor: Colors.white,
+                bottom: TabBar(
+                  dividerColor: Colors.black,
+                  tabs: [
+                    Container(
+                      width: 200,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 2, color: Colors.black)),
+                      child: const Tab(
+                        height: 30,
+                        child: Text(
+                          'Daily',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 2, color: Colors.black)),
+                      child: const Tab(
+                        height: 30,
+                        child: Text(
+                          'Upcoming 2 weeks',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              body: TabBarView(
+                children: [
+                  ForecastScreen(),
+                  UpcomingScreen(),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
